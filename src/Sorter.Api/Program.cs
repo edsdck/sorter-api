1980b1d101ae29ece1a5022aa02465460e7a89d6
Edvinas Sadeckas <edvinas.sadeckas@visma.com>
2022-05-13 19:28:19 +0300

    Setup API versioning

--- src/Sorter.Api/Program.cs
+++ src/Sorter.Api/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddApiVersioning(config =>
+{
+    config.DefaultApiVersion = new ApiVersion(1, 0);
+    config.AssumeDefaultVersionWhenUnspecified = true;
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
