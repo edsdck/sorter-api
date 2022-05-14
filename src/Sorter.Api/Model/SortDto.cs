f18bea937021e87e063d0ee52e8ca1a8adac1ae5
Edvinas Sadeckas <edvinas.sadeckas@visma.com>
2022-05-14 15:08:00 +0300

    Add validation to sort request model

--- src/Sorter.Api/Model/SortDto.cs
+++ src/Sorter.Api/Model/SortDto.cs
@@ -1,11 +1,16 @@
-﻿using Sorter.Core.Domain;
+﻿using System.ComponentModel.DataAnnotations;
+using Sorter.Core.Domain;
 
 namespace Sorter.Api.Model
 {
     public class SortDto
     {
+        [Required]
         public IList<long> Numbers { get; set; }
 
-        public SortAlgorithm SortAlgorithm { get; set; }
+        /// <summary>
+        /// defaults to bubble sort when algorithm is not provided by a client
+        /// </summary>
+        public SortAlgorithm SortAlgorithm { get; set; } = SortAlgorithm.BubbleSort;
     }
 }
