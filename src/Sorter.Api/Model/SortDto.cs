6a095aade0568275be2c8f484c1bb4ffe06f2b1c
Edvinas Sadeckas <edvinas.sadeckas@visma.com>
2022-05-14 17:14:02 +0300

    Add validation for sort algorithm value

--- src/Sorter.Api/Model/SortDto.cs
+++ src/Sorter.Api/Model/SortDto.cs
@@ -11,6 +11,7 @@ namespace Sorter.Api.Model
         /// <summary>
         /// defaults to bubble sort when algorithm is not provided by a client
         /// </summary>
+        [Range((int)SortAlgorithm.BubbleSort, (int)SortAlgorithm.SelectionSort)]
         public SortAlgorithm SortAlgorithm { get; set; } = SortAlgorithm.BubbleSort;
     }
 }
